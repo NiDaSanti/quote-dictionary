fc9de29680322eda18e4ad3e8339f3726c0a23c5
NiDaSanti <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>
2023-09-26 19:15:31 -0700

    created a new component to auto calculate quotes, but this is to be stored for now

---
+++ src/Components/QuoteCalculator/quoteCalculator.js
@@ -0,0 +1,14 @@
+/*Requires additional research for advanced version*/
+
+import React, {useState} from 'react'
+
+const QuoteCalculator = () => {
+  return(
+    <>
+      <div className="calculator-container">
+
+      </div>
+    </>
+  )
+}
+export default QuoteCalculator
