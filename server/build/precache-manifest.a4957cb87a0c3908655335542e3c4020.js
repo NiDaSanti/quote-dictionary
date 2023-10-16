5e3738508f5885a6443e6cafd3ed72bc981ee574
NiDaSanti <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>
2023-10-16 01:25:36 -0700

    updating orign main

---
+++ server/build/precache-manifest.a4957cb87a0c3908655335542e3c4020.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b83c1275d5997c91e580",
+    "url": "/static/css/main.01157cb4.chunk.css"
+  },
+  {
+    "revision": "b83c1275d5997c91e580",
+    "url": "/static/js/main.d3aa0148.chunk.js"
+  },
+  {
+    "revision": "f28614c1f22f71c9cbf5",
+    "url": "/static/js/runtime~main.3592e290.js"
+  },
+  {
+    "revision": "6d83dcf08fc77e2c9b9d",
+    "url": "/static/js/2.f561193f.chunk.js"
+  },
+  {
+    "revision": "bbb85af136b7dcd525ee",
+    "url": "/static/js/3.518f373d.chunk.js"
+  },
+  {
+    "revision": "84d6a5343b2a20d643b9eb1ba904c347",
+    "url": "/static/media/image2.84d6a534.png"
+  },
+  {
+    "revision": "eb6971bf1914bcaec3ea9289daab2dde",
+    "url": "/index.html"
+  }
+];
