30d6aafdff4d339480162733051bf737bc491ebd
NiDaSanti <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>
2023-10-31 17:50:55 -0700

    improved changes

--- src/Components/ClientForm/clientForm.js
+++ src/Components/ClientForm/clientForm.js
@@ -133,6 +133,7 @@ const ClientForm = ({formToogle, formOpenAndClose}) => {
         // Check if responseData.records is an array and has at least one item
         if (responseData.records && Array.isArray(responseData.records) && responseData.records.length > 0) {
           handleAddClient(responseData.records)
+          setIsLoadInProgress(false)
         } else {
           console.error('Invalid responseData.records:', responseData.records)
         }
